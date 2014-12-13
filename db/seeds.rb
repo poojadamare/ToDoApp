6059705e98dc1e65ed7bfd9514943b8314672b18
poojadamare <pooja.damare@gmail.com>
2014-12-13 16:49:37 +0530

    Added pg gem and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+  Task.create(:action => 'To deploy the application')
+  Task.create(:action => 'To showcase the application')
+  Task.create(:action => 'To be happy' , :is_completed => true )
